25c7ba6bab68cc7e5e488d5d4d089728f46be739
misharnet <misharnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-29 05:43:11 +0000

    First Upload.
    git-svn-id: https://plugins.svn.wordpress.org/ns-widget-recent-comments/trunk@1015696 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,76 @@
+=== NS Widget Recent Comments ===
+Contributors: Miodrag Rasic
+Tags: netscripter, recent comments, widget, avatar, free, recent comments with avatars
+Requires at least: 3.0
+Tested up to: 4
+Stable tag: trunk
+License: GNU General Public License
+License URI: http://www.gnu.org/licenses/
+ 
+  
+ 
+This is a widget that let you show the latest comments from your WordPress Blog in a Sidebar area with comments author's avatars.
+   
+
+== Description ==
+NS Widget Recent Comments is simple and effective plugin. There is only two, but most important options that you can choose:
+
+- Number of comments to show.
+- Size of author's avatar.
+
+Clean design makes it easy to fit in any kind of look of WordPress themes. 
+
+= Features =
+*	Show chosen number of recent comments from your blog with author's avatars.
+
+= Plugin Info =
+
+* Developed by [Miodrag Rasic] & [NetScripter](http://netscripter.me/)
+
+
+== Installation ==
+
+ - From The WordPress Dashboard
+
+    Navigate to the \'Add New\' in the plugins dashboard
+    Search for \'NS Widget Recent Comments\'
+    Click \'Install Now\'
+    Activate the plugin on the Plugin dashboard
+
+ - Uploading to your WordPress Dashboard
+
+    Navigate to the \'Add New\' in the plugins dashboard
+    Navigate to the \'Upload\' area
+    Select ns-widget-recent-comments.zip from your PC or Lap Top
+    Click \'Install Now\'
+    Activate the plugin in the Plugin dashboard
+
+ - Using FTP Client
+
+    Download ns-widget-recent-comments.zip
+    Extract the ns-tweet directory to your PC or Lap Top
+    Upload the ns-tweet directory to the /wp-content/plugins/ directory
+    Activate the plugin in the Plugin dashboard
+
+
+- Usage:
+
+On the Appearance dashboard choose Widgets. Find "NS Widget Recent Comments" on the left side where the widgets are. Click on it and choose the widget area you would like to place the widget. Then click on the small arrow on the right from the "NS Widget Recent Comments" title, a dropdown menu with 
+	
+options will appear. You can then fill in the informations and choose appropriate options as you like.
+
+Click "save" button, and your Recent Comments are on your WordPress blog's Sidebar!
+
+== Screenshots ==
+
+1. NS Widget Recent Comments options
+2. NS Widget Recent Comments
+
+
+== Frequently Asked Questions ==
+
+
+== Changelog ==
+
+This is completely brand new version of this plugin.
+
---
+++ widget-recent-comments.php
@@ -0,0 +1,126 @@
+<?php
+/*
+* Plugin Name: NS Widget Recent Comments
+* Plugin URI: http://netscripter.me/ns-widget-recent-comments
+* Author: NetScripter
+* Author URI: http://netscripter.me
+* Description: Add a recent comments widget that shows author's avatar.
+* Version: 1.0
+* Text Domain: netscripter
+*/
+
+function ns_init() {
+	register_widget('NS_Recent_Comments_');
+}
+add_action( 'widgets_init', 'ns_init' );
+
+
+function ns_scripts() {
+wp_enqueue_style( 'recent-comment', plugins_url('/css/widget-recent-comments.css', __FILE__) );
+wp_enqueue_style( 'recent-comments' );
+}
+add_action('wp_enqueue_scripts','ns_scripts');
+
+
+/* Recent_Comments widget class */
+
+class NS_Recent_Comments_ extends WP_Widget {
+
+	function NS_Recent_Comments_() {
+		$widget_ops = array('classname' => 'widget_ns', 'description' => __( 'Recent comments, with Avatars.' , 'netscripter') );
+		$this->WP_Widget('ns-recent-comments', __('NetScripter: Recent Comments', 'netscripter'), $widget_ops);
+		$this->alt_option_name = 'widget_ns';
+
+		if ( is_active_widget(false, false, $this->id_base) )
+			add_action( 'wp_head', array(&$this, 'widget_style') );
+
+		add_action( 'comment_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'transition_comment_status', array(&$this, 'flush_widget_cache') );
+	}
+
+	function widget_style() { ?>
+	
+<?php
+	}
+
+	function flush_widget_cache() {
+		wp_cache_delete('widget_ns', 'widget');
+	}
+
+	function widget( $args, $instance ) {
+		global $comments, $comment;
+
+		$cache = wp_cache_get('widget_ns', 'widget');
+
+		if ( ! is_array( $cache ) )
+			$cache = array();
+
+		if ( isset( $cache[$args['widget_id']] ) ) {
+			echo $cache[$args['widget_id']];
+			return;
+		}
+
+ 		extract($args, EXTR_SKIP);
+ 		$output = '';
+ 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Comments', 'netscripter') : $instance['title']);
+
+		if ( ! $number = (int) $instance['number'] )
+ 			$number = 5;
+ 		else if ( $number < 1 )
+ 			$number = 1;
+
+		$size = $instance['size'];
+
+		$comments = get_comments( array( 'number' => $number, 'status' => 'approve' ) );
+		$output .= $before_widget;
+		if ( $title )
+			$output .= $before_title . $title . $after_title;
+
+		$output .= '<ul id="ns">';
+		if ( $comments ) {
+			foreach ( (array) $comments as $comment) {
+				$output .= '<div class="avat">';
+				$output .=  get_avatar(get_comment_author_email($comment->comment_ID), $size) . ' ';
+				$output .=  sprintf(__('%1$s on </div><li class="ns-comment"> %2$s', 'netscripter'), get_comment_author(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a>');
+				$output .=  '<br style="clear:both;height:0;margin:0;padding:0;" /></li>';
+			}
+ 		}
+		$output .= '</ul>';
+		$output .= $after_widget;
+
+		echo $output;
+		$cache[$args['widget_id']] = $output;
+		wp_cache_set('widget_ns', $cache, 'widget');
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = (int) $new_instance['number'];
+		$instance['size'] = ( $new_instance['size'] < 20 ) ? 20 : (int) $new_instance['size'];
+		
+		$this->flush_widget_cache();
+
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset($alloptions['widget_ns']) )
+			delete_option('widget_ns');
+
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+		$number = isset($instance['number']) ? absint($instance['number']) : 3;
+		$size = isset($instance['size']) ? absint($instance['size']) : 40;
+?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'netscripter'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of comments to show:', 'netscripter'); ?></label>
+		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+		
+		<p><label for="<?php echo $this->get_field_id('size'); ?>"><?php _e('Avatar size:', 'netscripter'); ?></label>
+		<input id="<?php echo $this->get_field_id('size'); ?>" name="<?php echo $this->get_field_name('size'); ?>" type="text" value="<?php echo $size; ?>" size="3" /></p>
+<?php
+	}
+}
