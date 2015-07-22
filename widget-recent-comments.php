0a18fb8ea0786894c2b593df5e7b39f1ff43ccac
misharnet <misharnet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-22 19:07:31 +0000

    Changed Language file name.
    git-svn-id: https://plugins.svn.wordpress.org/ns-widget-recent-comments/trunk@1204230 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget-recent-comments.php
+++ widget-recent-comments.php
@@ -2,11 +2,11 @@
 /*
 * Plugin Name: NS Widget Recent Comments
 * Plugin URI: http://netscripter.me/ns-widget-recent-comments
-* Author: NetScripter
+* Author: widget-recent-comments
 * Author URI: http://netscripter.me
 * Description: Add a recent comments widget that shows author's avatar.
 * Version: 1.1
-* Text Domain: netscripter
+* Text Domain: widget-recent-comments
 */
 
 function ns_init() {
@@ -15,7 +15,7 @@ function ns_init() {
 add_action( 'widgets_init', 'ns_init' );
 
 function ns_recent_comments_init() {
-  load_plugin_textdomain( 'netscripter', false, 'ns-widget-recent-comments/languages' );
+  load_plugin_textdomain( 'widget-recent-comments', false, 'ns-widget-recent-comments/languages' );
 }
 add_action('init', 'ns_recent_comments_init');
 
@@ -31,8 +31,8 @@ add_action('wp_enqueue_scripts','ns_scripts');
 class NS_Recent_Comments_ extends WP_Widget {
 
 	function NS_Recent_Comments_() {
-		$widget_ops = array('classname' => 'widget_ns', 'description' => __( 'Recent comments, with Avatars.' , 'netscripter') );
-		$this->WP_Widget('ns-recent-comments', __('NS Recent Comments', 'netscripter'), $widget_ops);
+		$widget_ops = array('classname' => 'widget_ns', 'description' => __( 'Recent comments, with Avatars.' , 'widget-recent-comments') );
+		$this->WP_Widget('ns-recent-comments', __('NS Recent Comments', 'widget-recent-comments'), $widget_ops);
 		$this->alt_option_name = 'widget_ns';
 
 		if ( is_active_widget(false, false, $this->id_base) )
@@ -66,7 +66,7 @@ class NS_Recent_Comments_ extends WP_Widget {
 
  		extract($args, EXTR_SKIP);
  		$output = '';
- 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Comments', 'netscripter') : $instance['title']);
+ 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Comments', 'widget-recent-comments') : $instance['title']);
 
 		if ( ! $number = (int) $instance['number'] )
  			$number = 5;
@@ -85,7 +85,7 @@ class NS_Recent_Comments_ extends WP_Widget {
 			foreach ( (array) $comments as $comment) {
 				$output .= '<div class="avat">';
 				$output .=  get_avatar(get_comment_author_email($comment->comment_ID), $size) . ' ';
-				$output .=  sprintf(__('%1$s on </div><li class="ns-comment"> %2$s', 'netscripter'), get_comment_author(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a>');
+				$output .=  sprintf(__('%1$s on </div><li class="ns-comment"> %2$s', 'widget-recent-comments'), get_comment_author(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a>');
 				$output .=  '<br style="clear:both;height:0;margin:0;padding:0;" /></li>';
 			}
  		}
@@ -117,13 +117,13 @@ class NS_Recent_Comments_ extends WP_Widget {
 		$number = isset($instance['number']) ? absint($instance['number']) : 3;
 		$size = isset($instance['size']) ? absint($instance['size']) : 40;
 ?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'netscripter'); ?></label>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'widget-recent-comments'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
 
-		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of comments to show:', 'netscripter'); ?></label>
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of comments to show:', 'widget-recent-comments'); ?></label>
 		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
 		
-		<p><label for="<?php echo $this->get_field_id('size'); ?>"><?php _e('Avatar size:', 'netscripter'); ?></label>
+		<p><label for="<?php echo $this->get_field_id('size'); ?>"><?php _e('Avatar size:', 'widget-recent-comments'); ?></label>
 		<input id="<?php echo $this->get_field_id('size'); ?>" name="<?php echo $this->get_field_name('size'); ?>" type="text" value="<?php echo $size; ?>" size="3" /></p>
 <?php
 	}
